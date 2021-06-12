02e7de4fe630c43b4dd3481530517a7d04cc6fbf
geraldelorm <geraldelorm14@gmail.com>
2021-06-12 19:17:22 +0000

    :tada: done

--- js/app.js
+++ js/app.js
@@ -1,282 +1,107 @@
+const form = document.getElementById('input_area');
 const input = document.getElementById('input');
-const check = document.getElementById('check');
-const provider = document.getElementById('provider');
 const cardNumber = document.getElementById('card_number');
-const result = document.getElementById('result');
-const length = input.value.replace(/\s+/g, '').length;
 
-check.addEventListener('click', () => {
-  checkInputValidity();
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
 });
 
-// Check Card Number Length if its 13, 15, 16 or 19
-function checkInputValidity() {
-  const length = input.value.replace(/\s+/g, '').length;
-  switch (true) {
-    case length === 0:
+input.addEventListener('keyup', updateValue);
+
+input.addEventListener('change', updateValueAndSpaceOut);
+
+function updateValue(e) {
+  cardNumber.textContent = e.target.value;
+}
+function updateValueAndSpaceOut(e) {
+  e.target.value = spaceOut(e.target.value);
+  cardNumber.textContent = e.target.value;
+
+  cleanInput();
+}
+const spaceOut = (arr) => {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (i == 3 || i == 7 || i == 11 || i == 15) {
+      newArr.push(arr[i] + ' ');
+    } else {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr.join('').toString();
+};
+
+const cleanInput = function () {
+  const cleanedInput = input.value.replace(/\s+/g, '');
+  let length = input.value.replace(/\s+/g, '').length;
+
+  switch (length) {
+    case 0:
       result.innerHTML = `<h1>Enter A Card Number</h1>`;
+      provider.style.backgroundImage = 'url(./assets/svg/unknown-folder.svg)';
       break;
-    case length === 13:
-    case length === 15:
-    case length === 16:
-    case length === 19:
-      luhnCheck(input.value.replace(/\s+/g, ''));
+    case 13:
+    case 15:
+    case 16:
+    case 19:
+      if (luhnCheck(cleanedInput)) {
+        checkProvider();
+      } else {
+        result.innerHTML = `<h1>Invalid Card Number</h1>`;
+        provider.style.backgroundImage = 'url(./assets/svg/unknown-folder.svg)';
+      }
       break;
     default:
       result.innerHTML = `<h1>Invalid Length</h1>`;
       provider.style.backgroundImage = 'url(./assets/svg/unknown-folder.svg)';
-      cardNumber.innerText = newNum;
-  }
-}
-
-// Luhn Algorithm
-const luhnCheck = (num) => {
-  let arr = (num + '')
-    .split('')
-    .reverse()
-    .map((x) => parseInt(x));
-  let lastDigit = arr.splice(0, 1)[0];
-  let sum = arr.reduce((acc, val, i) => (i % 2 !== 0 ? acc + val : acc + ((val * 2) % 9) || 9), 0);
-  sum += lastDigit;
-  //  To space out numbers for card display
-  var displayNum = (num + '').split('');
-  const newArr = [];
-  for (let i = 0; i < displayNum.length; i++) {
-    if (i == 3 || i == 7 || i == 11 || i == 15) {
-      newArr.push(displayNum[i] + ' ');
-    } else {
-      newArr.push(displayNum[i]);
-    }
-  }
-  var newNum = newArr.join('');
-  if (sum % 10 !== 0) {
-    result.innerHTML = `<h1>Your Card Is Invalid</h1>`;
-    provider.style.backgroundImage = 'url(./assets/svg/unknown-folder.svg)';
-    cardNumber.innerText = newNum;
-  } else {
-    let newArr = (num + '').split('').map((x) => parseInt(x));
-    if (newArr[0] == 4) {
-      if (
-        input.value.replace(/\s+/g, '').length == 13 ||
-        input.value.replace(/\s+/g, '').length == 16
-      ) {
-        result.innerHTML = `<h1>Your Card Is A VISA Card</h1>`;
-        provider.style.backgroundImage = 'url(./assets/svg/visa.svg)';
-        cardNumber.innerText = newNum;
-      }
-    } else if (newArr[0] == 5 && input.value.replace(/\s+/g, '').length == 16) {
-      result.innerHTML = `<h1>Your Card Is A Mastercard</h1>`;
-      provider.style.backgroundImage = 'url(./assets/svg/mastercard.svg)';
-      cardNumber.innerText = newNum;
-      // } else if (newArr[0] == 2 && input.value.replace(/\s+/g, '').length == 16) {
-      //   result.innerHTML = `<h1>Your Card Is A Mastercard</h1>`;
-      //   provider.style.backgroundImage = 'url(./assets/svg/mastercard.svg)';
-      //   cardNumber.innerText = newNum;
-    } else if (newArr[0] == 3 && input.value.replace(/\s+/g, '').length == 15) {
-      if (newArr[1] == 4 || newArr[1] == 7) {
-        result.innerHTML = `<h1>Your Card Is An American Express Card</h1>`;
-        provider.style.backgroundImage = 'url(./assets/svg/american-express.svg)';
-        cardNumber.innerText = newNum;
-      }
-    } else if (newArr[0] == 6 && input.value.replace(/\s+/g, '').length == 16) {
-      result.innerHTML = `<h1>Your Card Is A Discover Card</h1>`;
-      provider.style.backgroundImage = 'url(./assets/svg/discover.svg)';
-      cardNumber.innerText = newNum;
-    } else {
-      result.innerHTML = `<h1>This Card Provider Is Unknown</h1>`;
-      provider.style.backgroundImage = 'url(./assets/svg/unknown-folder.svg)';
-      cardNumber.innerText = newNum;
-    }
   }
 };
 
-// const CreditCard = () => {
-//   const [creditCardNumber, setCreditCardNumber] = useState('');
-//   const [creditCardHolderName, setCreditCardHolderName] = useState('CS50 Credit Card');
+const luhnCheck = function (value) {
+  // Accept only digits, dashes or spaces
+  if (/[^0-9-\s]+/.test(value)) return false;
 
-//   const visaImageLink =
-//     'https://raw.githubusercontent.com/muhammederdem/credit-card-form/master/src/assets/images/visa.png';
+  // The Luhn Algorithm.
+  let nCheck = 0,
+    bEven = false;
+  value = value.replace(/\D/g, '');
 
-//   const masterCardImageLink =
-//     'https://upload.wikimedia.org/wikipedia/commons/a/a4/Mastercard_2019_logo.svg';
-//   const amexCardImageLink = 'https://cdn.icon-icons.com/icons2/1178/PNG/512/amex_82052.png';
+  for (var n = value.length - 1; n >= 0; n--) {
+    var cDigit = value.charAt(n),
+      nDigit = parseInt(cDigit, 10);
 
-//   const inValidImageLink =
-//     'https://previews.123rf.com/images/chrisdorney/chrisdorney1411/chrisdorney141100155/34073570-invalid-red-rubber-stamp-over-a-white-background-.jpg';
+    if (bEven && (nDigit *= 2) > 9) nDigit -= 9;
 
-//   const type = cardType(creditCardNumber);
+    nCheck += nDigit;
+    bEven = !bEven;
+  }
 
-//   const cardImage = (creditCardNumber) => {
-//     if (type === 'Invalid') {
-//       return inValidImageLink;
-//     } else if (type === 'AMEX') {
-//       return amexCardImageLink;
-//     } else if (type === 'Master Card') {
-//       return masterCardImageLink;
-//     } else if (type === 'VISA') {
-//       return visaImageLink;
-//     }
-//   };
-//   const addSpaceAfterFour = (number) => {
-//     number = number.toString();
-//     return number.match(/.{1,4}/g).join(' ');
-//   };
+  return nCheck % 10 == 0;
+};
 
-//   // Get the Credit card number
+const checkProvider = function () {
+  const cleanedInput = input.value.replace(/\s+/g, '');
+  let length = input.value.replace(/\s+/g, '').length;
+  let inputArray = cleanedInput.split('');
 
-//   function checksum(creditCardNumber) {
-//     //   console.log(creditCardNumber);
-
-//     // Iterate over the creditCardNumber
-//     creditCardNumber = reverseString(creditCardNumber.toString());
-//     let secondToLastSum = 0;
-//     for (var i = 0; i < creditCardNumber.length; i++) {
-//       if ((i + 1) % 2 === 0) {
-//         if ((parseInt(creditCardNumber[i]) * 2).toString().length === 2) {
-//           // console.log(parseInt(creditCardNumber[i]) * 2)
-//           secondToLastSum += sumOfString(parseInt(creditCardNumber[i]) * 2);
-//         } else {
-//           secondToLastSum += parseInt(creditCardNumber[i] * 2);
-//         }
-//       } else {
-//         secondToLastSum += parseInt(creditCardNumber[i]);
-//       }
-//     }
-//     //   console.log(secondToLastSum)
-//     return secondToLastSum % 2 === 0;
-//   }
-
-//   // console.log(checksum(4003600000000014));
-//   // console.log(secondToLastSum)
-
-//   function sumOfString(num) {
-//     num = num.toString();
-//     let sum = 0;
-//     for (var i = 0; i < num.length; i++) {
-//       sum += parseInt(num[i]);
-//     }
-//     return sum;
-//   }
-
-//   // console.log(sumOfString(1))
-//   // Reverse string
-//   function reverseString(str) {
-//     if (str === '') return '';
-//     else return reverseString(str.substr(1)) + str.charAt(0);
-//   }
-
-//   function cardType(creditCardNumber) {
-//     if (typeof parseInt(creditCardNumber) !== 'number') {
-//       return 'Invalid';
-//     }
-
-//     creditCardNumber = creditCardNumber.toString();
-//     //   console.log("Credit Card Length: " + creditCardNumber.length);
-
-//     if (checksum(creditCardNumber) === true) {
-//       // console.log("Check sum pass");
-
-//       // Check For AMEX
-//       var firstTwoChar = creditCardNumber.toString().substring(0, 2);
-//       // console.log("First Two Char: " + firstTwoChar);
-//       const masterCardTwoValid =
-//         firstTwoChar === '51' ||
-//         firstTwoChar === '52' ||
-//         firstTwoChar === '53' ||
-//         firstTwoChar === '54' ||
-//         firstTwoChar === '55';
-//       const amexTwoValid = firstTwoChar === '34' || firstTwoChar === '37';
-//       const visaTwoValid = creditCardNumber[0] === '4';
-//       const visaCardLenValid = creditCardNumber.length === 13 || creditCardNumber.length === 16;
-//       // console.log("Master Card Two Valid: " + masterCardTwoValid);
-//       // console.log("Amex Card Two Valid: " + amexTwoValid);
-//       // console.log("Visa Card Two Valid: " + visaTwoValid);
-//       // console.log("Visa Card Len Valid: " + visaCardLenValid);
-
-//       if (creditCardNumber.length === 15 && amexTwoValid) {
-//         //   console.log("AMEX");
-//         return 'AMEX';
-//       } else if (creditCardNumber.length === 16 && masterCardTwoValid) {
-//         console.log('Master Card');
-//         return 'Master Card';
-//       } else if (visaCardLenValid && visaTwoValid) {
-//         //   console.log("VISA");
-//         return 'VISA';
-//       } else {
-//         //   console.log("Invalid");
-//         return 'Invalid';
-//       }
-//     } else {
-//       // console.log("Invalid");
-//       return 'Invalid';
-//     }
-//   }
-
-//   const handleCreditCardHolderNameChange = (event) => {
-//     setCreditCardHolderName(event.target.value);
-//   };
-
-//   const handleCreditCardNumberChange = (event) => {
-//     setCreditCardNumber(event.target.value);
-//   };
-//   return (
-//     <div>
-//       <Card variant='outlined' style={cardStyle}>
-//         <div className='firstRow' style={firstRowStyle}>
-//           <div className='chipImage' style={chipImageStyle}>
-//             <img
-//               style={{ width: 50 }}
-//               alt='chip'
-//               src={
-//                 'https://raw.githubusercontent.com/muhammederdem/credit-card-form/master/src/assets/images/chip.png'
-//               }
-//             />
-//           </div>
-//           <div className='cardName' style={cardNameStyle}>
-//             <img
-//               src={cardImage(creditCardNumber)}
-//               alt='visaImageLink'
-//               style={{ width: 100, marginRight: 10 }}
-//             />
-//           </div>
-//         </div>
-//         <div className='secondRow' style={secondRowStyle}>
-//           <div style={{ fontSize: 30 }}>
-//             {creditCardNumber === '' ? '#### #### #### ####' : addSpaceAfterFour(creditCardNumber)}
-//           </div>
-//         </div>
-//         <div className='thirdRow' style={thirdRowStyle}>
-//           <div style={{ fontSize: 30 }}>
-//             {creditCardHolderName === '' ? 'CS50 Credit Card' : creditCardHolderName}
-//           </div>
-//         </div>
-//       </Card>
-
-//       <Card style={cardFormStyle}>
-//         <div style={{ margin: 'auto', marginTop: 20 }}>
-//           <form>
-//             <TextField
-//               id='filled-basic'
-//               label='Credit Card Number'
-//               variant='filled'
-//               value={creditCardNumber}
-//               onChange={handleCreditCardNumberChange}
-//             />
-//           </form>
-//         </div>
-//         <div style={{ margin: 'auto', marginTop: 20, marginBottom: 30 }}>
-//           <form>
-//             <TextField
-//               id='filled-basic'
-//               label='Card Holder'
-//               variant='outlined'
-//               color='primary'
-//               value={creditCardHolderName}
-//               onChange={handleCreditCardHolderNameChange}
-//             />
-//           </form>
-//         </div>
-//       </Card>
-//     </div>
-//   );
-// };
+  if (inputArray[0] == 4) {
+    if (length == 13 || length == 16) {
+      result.innerHTML = `<h1>Your Card Is A VISA Card</h1>`;
+      provider.style.backgroundImage = 'url(./assets/svg/visa.svg)';
+    }
+  } else if (inputArray[0] == 5 && length == 16) {
+    result.innerHTML = `<h1>Your Card Is A Mastercard</h1>`;
+    provider.style.backgroundImage = 'url(./assets/svg/mastercard.svg)';
+  } else if (inputArray[0] == 3 && length == 15) {
+    if (inputArray[1] == 4 || inputArray[1] == 7) {
+      result.innerHTML = `<h1>Your Card Is An Amex Card</h1>`;
+      provider.style.backgroundImage = 'url(./assets/svg/american-express.svg)';
+    }
+  } else if (inputArray[0] == 6 && length == 16) {
+    result.innerHTML = `<h1>Your Card Is A Discover card</h1>`;
+    provider.style.backgroundImage = 'url(./assets/svg/discover.svg)';
+  } else {
+    result.innerHTML = `<h1>Unknown Card Provider</h1>`;
+    provider.style.backgroundImage = 'url(./assets/svg/unknown-folder.svg)';
+  }
+};
